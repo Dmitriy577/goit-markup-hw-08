14d9004a3ec0a9f2af702aab86310735102b2753
Dmitriy577 <51756301+Dmitriy577@users.noreply.github.com>
2022-05-18 14:19:11 +0300

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,31 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+  
+  // бургер
+  
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modale-open]"),
+      closeModalBtn: document.querySelector("[data-modale-close]"),
+      modal: document.querySelector("[data-modale]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("modale");
+    }
+  })();
